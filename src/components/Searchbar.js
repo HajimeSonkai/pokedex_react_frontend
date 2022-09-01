7b566efbc39e2197be21d4a2fc84f69f1a2d1781
HajimeSonkai <mfilho86@gmail.com>
2022-09-01 20:05:47 -0300

    Adiciona as informações e ao Searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,17 +1,22 @@
 import "./Searchbar.css";
 import React from "react";
 import { useState } from "react";
+import { searchpokemon } from "../ApiConections.js/api";
 
 const Searchbar = () => {
   const [search, setSearch] = useState();
+  const [pokemondata, setPokemondata] = useState();
 
   const onChangeHandler = (e) => {
     console.log("onChange:", e.target.value);
     setSearch(e.target.value);
   };
 
-  const onClickHandler = () => {
+  const onClickHandler = async () => {
     console.log("procurando:", search);
+    const result = await searchpokemon(search);
+    setPokemondata(result);
+    console.log("pokeAPI achou: ", result);
   };
 
   return (
@@ -22,6 +27,14 @@ const Searchbar = () => {
       <div className="searchbar-btn">
         <button onClick={onClickHandler}>Buscar</button>
       </div>
+      {pokemondata ? (
+        <div>
+          <div>nome: {pokemondata.name}</div>
+          <div>numero: {pokemondata.id}</div>
+          <div>peso: {pokemondata.weight}</div>
+          <img src={pokemondata.sprites.front_default} alt={pokemondata.name} />
+        </div>
+      ) : null}
     </div>
   );
 };
