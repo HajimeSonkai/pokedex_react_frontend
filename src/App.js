da9a97413a25b678da1fad9c2be2a5b50907844e
HajimeSonkai <mfilho86@gmail.com>
2022-09-08 14:49:23 -0300

    adiona o fetch da lista

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { getpokemons } from "./ApiConections.js/api";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Pokedex from "./components/Pokedex";
 import Searchbar from "./components/Searchbar";
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [pokemons, setPokemons] = useState([])
+
+  const fetchApi = async () => {
+    try {
+      setLoading(true)
+      const result = await getpokemons()
+      setPokemons(result)
+      setLoading(false)
+      console.log("fetch result:", result)
+    } catch (error) {
+      console.log("getPokemon Error:", error)
+    }
+  }
+
+  useEffect(() => {
+    console.log("Carregando")
+    fetchApi();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <Pokedex />
+      <Pokedex pokemons={pokemons.results} loading={loading} />
     </div>
   );
 }
