bf641491311441579f9d303a38cda008ca048d0a
HajimeSonkai <mfilho86@gmail.com>
2022-09-08 16:20:29 -0300

    atualiza o card com as informações especificas

--- src/components/Pokemoncard.js
+++ src/components/Pokemoncard.js
@@ -1,12 +1,44 @@
 import React from "react";
 
 const Pokemoncard = (props) => {
-    const { pokemon} = props;
-    return (
-        <div>
-            <div>{pokemon.name}</div>
-        </div>
-    )
-}
+  const { pokemon } = props;
+  const heart = "❤️"
+  const clickheart = () => {
+    console.log("favoritando")
+  }
 
-export default Pokemoncard
+  return (
+    <div className="pokemon-card">
+
+      <div className="pokemon-img-div">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      </div>
+
+      <div className="card-body">
+
+        <div className="card-top">
+          <h3>{pokemon.name}</h3>
+          <div>numero: {pokemon.id}</div>
+        </div>
+
+        <div className="card-botton">
+
+          <div className="pokemon-type">
+            {pokemon.types.map((type, index) => {
+              return (
+                <div key={index} className="pokemon-type-txt">
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
+            <button className="fav-button" onClick={clickheart}>{heart}</button>
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Pokemoncard;
