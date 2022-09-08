c84a7e4f333c51a4aae7bbc0ec9c59385106421c
HajimeSonkai <mfilho86@gmail.com>
2022-09-08 14:49:40 -0300

    adicona o card na pokedex

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,15 +1,30 @@
 import React from "react";
-import "./Pokedex.css"
+import "./Pokedex.css";
+import Pokemoncard from "./Pokemoncard";
 
-const Pokedex = () => {
+const Pokedex = (props) => {
+  const { pokemons, loading } = props;
+  console.log("pokemons carregados:", pokemons)
   return (
     <div>
       <div className="pokedex-header">
-      <h1>Pokedex</h1>
-      <div>paginação</div>
+        <h1>Pokedex</h1>
+        <div>paginação</div>
       </div>
+      {loading ? (
+        <div>carregando agora </div>
+      ) : (
+        <div className="pokedex-grid">
+          {pokemons &&
+            pokemons.map((pokemon, index) => {
+              return (
+                  <Pokemoncard key={index} pokemon={pokemon} />
+              );
+            })}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Pokedex
+export default Pokedex;
